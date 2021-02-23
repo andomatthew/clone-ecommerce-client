d77eaafc27a10556ca4c6faed6b8a2d250d43b24
andomatthew <alexandro.matthew@gmail.com>
2021-02-23 13:12:26 +0800

    layouting and read products before login

---
+++ client/src/components/Card.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <md-list-item>
+      <!--Card-->
+      <md-card>
+        <md-card-content>
+          <img :src="product.image_url">
+        </md-card-content>
+      </md-card>
+      <!--card-->
+      <div class="md-list-item-text">
+        <span>{{product.name}}</span>
+        <span>{{product.price}}</span>
+        <p>{{product.stocks}}</p>
+      </div>
+    </md-list-item>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: ['product']
+}
+</script>
+
+<style>
+img {
+  max-height: 196px;
+}
+
+</style>
--- client/src/views/Root.vue
+++ client/src/views/Root.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
     <md-tabs md-alignment="centered">
-      <md-tab  md-label="Home" md-icon="home"></md-tab>
+      <md-tab  md-label="Home" md-icon="home">
+        <md-list class="md-triple-line"
+          v-for="product in products" :key="product.id"
+        >
+          <Card :product="product"/>
+          <md-divider class="md-inset"></md-divider>
+        </md-list>
+      </md-tab>
       <md-tab  md-label="Login" md-icon="login"></md-tab>
       <md-tab  md-label="Register" md-icon="app_registration"></md-tab>
     </md-tabs>
@@ -9,11 +16,20 @@
 </template>
 
 <script>
+import Card from '../components/Card.vue'
 export default {
   name: 'Root',
+  components: {
+    Card
+  },
   data () {
     return {
 
+    }
+  },
+  computed: {
+    products () {
+      return this.$store.state.products
     }
   },
   created () {
